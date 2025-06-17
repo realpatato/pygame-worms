311e77dccc9168d1f532c029be6b69c4c660e6fc
realpatato <you@example.com>
2025-06-17 13:17:00 -0400

    moved explosion stuff to the explosion class

--- explosion.py
+++ explosion.py
@@ -22,5 +22,28 @@ class Explosion(pygame.sprite.Sprite):
         self._rect = pygame.Rect(0, 0, r * 2, r * 2)
         self._rect.center = (x, y)
     
+    def gen_overlap(self, level, screen):
+        level_parts_masks = level._foreground._masks
+        for i in range(len(level_parts_masks)):
+            level_part = level_parts_masks[i]
+            empty_surface = pygame.Surface((screen.get_width(), screen.get_height()), pygame.SRCALPHA)
+            overlap_mask = self._mask.overlap_mask(level_parts_masks[i], (level_part.get_rect().x - self._rect.x, level_part.get_rect().y - self._rect.y))
+            return overlap_mask.to_surface(surface=empty_surface, unsetcolor=(0, 0, 0, 0), setcolor=(255, 255, 255), dest=(self._rect.x, self._rect.y))
+
+    def remove_pixels(self, level, overlap):
+        overlap_pixels = pygame.PixelArray(overlap)
+        level_pixels = pygame.PixelArray(level._foreground._img)
+        for i in range(len(level_pixels)):
+            for k in range(len(level_pixels[i])):
+                if level_pixels[i][k] != 0 and overlap_pixels[i][k] != 0:
+                    level_pixels[i][k] = pygame.Color(0, 0, 0, 0)
+        del overlap_pixels
+        del level_pixels
+    
+    def explode(self, level, screen):
+        self.draw_self(screen)
+        overlap = self.gen_overlap(level, screen)
+        self.remove_pixels(level, overlap)
+    
     def draw_self(self, surface):
         surface.blit(self._draw_mask, (self._rect.x, self._rect.y))
--- main.py
+++ main.py
@@ -30,14 +30,13 @@ keep_playing = True
 clock = pygame.time.Clock()
 
 #loading and scaling the background
-background = pygame.image.load("levels/worm-level.png")
+background = pygame.image.load("levels/pygame-logo-level.png")
 background = pygame.transform.scale(background, (screen_width, screen_height))
 
 #creates a level object with the background image and screen
 level = lm.Level(background, screen)
 
 explosions = []
-overlaps = []
 
 clicked_and_touched = False
 
@@ -74,30 +73,11 @@ while keep_playing:
     level.draw_self()
 
     for explosion in explosions:
-        level_parts_masks = level._foreground._masks
-        level_parts_rects = level._foreground._rects
-        for i in range(len(level_parts_masks)):
-            level_part = level_parts_masks[i]
-            empty_surface = pygame.Surface((screen.get_width(), screen.get_height()), pygame.SRCALPHA)
-            overlap_mask = explosion._mask.overlap_mask(level_parts_masks[i], (level_part.get_rect().x - explosion._rect.x, level_part.get_rect().y - explosion._rect.y))
-            overlap_mask_surface = overlap_mask.to_surface(surface=empty_surface, unsetcolor=(0, 0, 0, 0), setcolor=(255, 255, 255), dest=(explosion._rect.x, explosion._rect.y))
-            overlaps.append(overlap_mask_surface)
-            try:
-                explosions.remove(explosion)
-            except:
-                pass
-
-    for overlap in overlaps:
-        screen.blit(overlap, (0, 0))
-        overlap_pixels = pygame.PixelArray(overlap)
-        level_part_pixels = pygame.PixelArray(level._foreground._img)
-        for i in range(len(level_part_pixels)):
-            for k in range(len(level_part_pixels[i])):
-                if level_part_pixels[i][k] != 0 and overlap_pixels[i][k] != 0:
-                    level_part_pixels[i][k] = pygame.Color(0, 0, 0, 0)
-        overlaps.remove(overlap)
-        del overlap_pixels
-        del level_part_pixels
+        explosion.explode(level, screen)
+        try:
+            explosions.remove(explosion)
+        except:
+            pass
 
     #updates the screen
     pygame.display.update()
